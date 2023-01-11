85ef8943f3685bd3725cc761cdc7080882750e18
otmanTR <r.brk.otman@gmail.com>
2023-01-11 18:28:02 +0300

    updated home desktop

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const speakers = [
-  {
-    speakersImages: ['bersoy.webp', 'chuck_palahniuk_kapak_2-620x375.jpg'],
-    speakersName: ['Bülent Ersoy', 'Chuck Palahniuk'],
-    speakersTitle: ['Turkish Diva', 'Writer the Fight Club'],
-    speakersTopic: ['How similar fantasy literature and fantasy music', 'A different look into Middle-earth'],
-  },
-];
+// const speakers = [
+//   {
+//     speakersImages: ['bersoy.webp', 'chuck_palahniuk_kapak_2-620x375.jpg'],
+//     speakersName: ['Bülent Ersoy', 'Chuck Palahniuk'],
+//     speakersTitle: ['Turkish Diva', 'Writer the Fight Club'],
+//     speakersTopic: ['How similar fantasy literature and fantasy music', 'A different look into Middle-earth'],
+//   },
+// ];
 
+// document.getElementById('hamburgericon').onclick = () => {
+// document.getElementById('speakerImage').src = speakers[0].speakerImages[0];
+// document.getElementById('speakerName').innerHTML = speakers[0].speakersName[0];
+// document.getElementById('speakerTitle').innerHTML = speakers[0].speakersTitle[0];
+// document.getElementById('speakerTopic').innerHTML = speakers[0].speakersTopic[0];
+// }
 
-document.getElementById('hamburgericon').onclick = () => {
-document.getElementById('speakerImage').src = speakers[0].speakerImages[0];
-document.getElementById('speakerName').innerHTML = speakers[0].speakersName[0];
-document.getElementById('speakerTitle').innerHTML = speakers[0].speakersTitle[0];
-document.getElementById('speakerTopic').innerHTML = speakers[0].speakersTopic[0];
-}
+// popUp
+// document.getElementById('hamburgericon').onclick = () => {
+//   document.getElementById('popUpItems').classList.add('hamburgericon');
+//   document.getElementById('hamburgericon').classList.add('popUp');
+//   document.getElementById('popUpCencel').classListremove('popUp')
+// };
